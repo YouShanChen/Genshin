e5b5cb8929f6958d86c6932f355c19651e872dec
YouShanChen <04071975le@gmail.com>
2023-05-24 18:54:41 +0800

    5/24_1854

--- src/components/News/index.jsx
+++ src/components/News/index.jsx
@@ -1,14 +1,18 @@
 import styles from "./news.module.css";
 
+
+
 export default function News() {
+
+
     return (
         <div className="container">
             <div className={styles.box}>
                 <div className={styles.title}>
                     最新活動
                 </div>
-                <div className={styles.imgbox}>
-
+                <div className={styles.imageBox}>
+                    <img src="/image/news1.jpg" alt="news" />
                 </div>
             </div>
         </div>
